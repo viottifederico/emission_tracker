6989811e0eb1737cdd9f58d7429a480a0bed8804
your_username <elidorv@outlook.com>
2023-11-07 12:48:35 +0100

    Emulated yearly training for two different models

--- scripts/logistic_regression_iris_train.py
+++ scripts/logistic_regression_iris_train.py
@@ -1,7 +1,5 @@
-#Importing dataset from sklearn
 from sklearn import datasets
 from sklearn import metrics
-#Splitting dataset into Training (80%) and testing data (20%) using train_test_split
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 from codecarbon import track_emissions
@@ -15,12 +13,12 @@ def fit_classifier(x, y, clf):
 
 if __name__ == "__main__":
     clf = LogisticRegression()
-    iris = datasets.load_iris() #dataset loading
-    X = iris.data               #Features stored in X 
-    y = iris.target             #Class variable
-    # Emulate training for a whole year
+    iris = datasets.load_iris() 
+    X = iris.data               
+    y = iris.target             
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
     # Emulate training for a whole year
     for i in range(365):
         trained_clf = fit_classifier(X_train, y_train, clf)
--- scripts/xgboost_iris_train.py
+++ scripts/xgboost_iris_train.py
@@ -1,4 +1,3 @@
-#Importing dataset from sklearn
 from sklearn import datasets
 from sklearn import metrics
 from sklearn import svm
@@ -6,11 +5,6 @@ from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
 
 from codecarbon import track_emissions
-
-
-
-
-#Splitting dataset into Training (80%) and testing data (20%) using train_test_split
 from sklearn.model_selection import train_test_split
 
 @track_emissions(project_name='XGBoost model', output_file='yearly_log.csv')
@@ -33,12 +27,13 @@ def fit_classifier(x, y, clf):
 if __name__ == "__main__":
     clf = XGBClassifier()
 
-    iris = datasets.load_iris() #dataset loading
-    X = iris.data               #Features stored in X 
-    y = iris.target             #Class variable
-    # Emulate training for a whole year
+    iris = datasets.load_iris()
+    X = iris.data                
+    y = iris.target             
+    
 
     X_train, X_test, y_train, y_test = split_data(X,y)
 
+    # Emulate training for a whole year
     for i in range(365):
         trained_clf = fit_classifier(X_train, y_train, clf)
