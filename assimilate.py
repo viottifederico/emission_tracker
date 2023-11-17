e8e9d18a7d6aa7497912206aeaa10c344cbd5096
Marco Gambetta <marco.gambetta@gmail.com>
2023-11-17 15:04:06 +0100

    versione 17-11 prelim

---
+++ assimilate.py
@@ -0,0 +1,65 @@
+"""
+ASSIMILATE.py
+
+Description : utility to preprocess raw data whose aim is to create input data to be used by streamlit objects
+"""
+import argparse
+import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta
+
+#init
+__appname__ = 'Assimilate.py'
+__version__ = "1.00.17.11.23"
+__verbosity__ = 1
+if __verbosity__ >= 1 : print('\n'*10+'\t{0:>30s} : {1:<50s}\n'.format(__appname__, __version__))
+
+#configure argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", type=str, help="Input Filename - fileformat",required = False)
+parser.add_argument("-fld", type=str, default = './assimilated_data', help="Local folder for assimilated data storage",required = False)
+
+args = parser.parse_args()
+
+#define raw data source (local csv file)
+if args.f is None: 
+    args.f = "data_raw/yearly_log copy.csv"
+if __verbosity__ >= 1 : print('\t{0:>30s} : {1:<50s}'.format('Source filename', args.f))
+
+#Source data assimilation, with columns preservation (hardcoded)
+raw_df = pd.read_csv(args.f)
+colums_to_preserve = ['timestamp','project_name','duration','emissions','emissions_rate','cpu_energy','cpu_power','ram_power','ram_energy','energy_consumed',
+                      'longitude','latitude']
+raw_df = raw_df[colums_to_preserve]
+if __verbosity__ >= 1 : print('\t{0:>30s} : {1:<6d}'.format('columns', len(colums_to_preserve)))
+if __verbosity__ >= 1 : print('\t{0:>30s} : {1:<6d}'.format('rows', len(raw_df.index)))
+
+# Value harmoziation for rendering
+raw_df['duration'] = raw_df['duration'] * 100
+raw_df['emissions'] = raw_df['emissions'] * 100000000
+raw_df['emissions_rate'] = raw_df['emissions_rate'] * 1000000
+raw_df['cpu_energy'] = raw_df['cpu_energy'] * 1000000
+raw_df['ram_energy'] = raw_df['ram_energy'] * 1000000
+raw_df['energy_consumed'] = raw_df['energy_consumed'] * 1000000
+
+# create a datetime colum so to have a single daily entry for each project 
+# Group by 'project_name' and add a 'date' column
+raw_df['date'] = pd.to_datetime(raw_df.groupby('project_name').cumcount().apply(lambda x: datetime(2022, 1, 1) + timedelta(days=x)))
+raw_df.pop('timestamp')
+
+#compute sum of all numeric columns and store in a new df
+Totals_df= raw_df.groupby('project_name').agg({'duration': 'sum','emissions': 'sum','emissions_rate': 'sum','cpu_power': 'sum',
+                                             'ram_power': 'sum','cpu_energy': 'sum','ram_energy': 'sum','energy_consumed': 'sum'}).reset_index()
+
+Totals_df.loc[len(Totals_df)] =  ['All_Projects']+Totals_df.sum(numeric_only=True, axis=0).tolist()
+if __verbosity__ >= 1 : print ('\nTotals  for all data grouped by project\n','-'*34,'\n',Totals_df)
+
+#create CSVs
+Totals_df.to_csv(args.fld+'/Totals.csv')
+if __verbosity__ >= 1: print('\n\t{0:>30s} | {1:<16s} \n\t{2:>30s} | {3:<16s} '.format('Project name','duration [days]','-'*30,'-'*16))
+for project_name in raw_df['project_name'].unique():
+    raw_df[raw_df['project_name']==project_name][:].to_csv(args.fld+'/'+project_name+'.csv')
+    if __verbosity__ >= 1: print('\t{0:>30s} | {1:<16d}'.format(project_name,len(raw_df[raw_df['project_name']==project_name])))
+
+#completion warning
+if __verbosity__ >= 1: print ('\n Process completed.')
--- streamlit_app/main_page.py
+++ streamlit_app/main_page.py
@@ -1,18 +1,34 @@
 import streamlit as st
 from datetime import date
 import pandas as pd
+import os
+
+#debug - verbosity level for printout
+versobisty = 1
 
 st.title("ALL PROJECTS")
-#st.markdown("# Main page 🎈")
-#st.sidebar.markdown("# Main page 🎈")
+st.markdown("# Main page 🎈")
+# st.sidebar.markdown("# Main page sidebar 🎈")
+st.image("CF.jpg", use_column_width=True)
 
+df_name = "./data_processed/whole_project/grouped_all_wildcard_df.csv"
+df = pd.read_csv(df_name)
+#echo to consolle - data source contents
+if versobisty >1:
+    print ('\n'*2,df_name,os.path.isfile(df_name))
+    print (df.head(),'\n'*2,df.describe())
 
-df = pd.read_csv("./data_processed/whole_project/grouped_all_wildcard_df.csv")
-series = pd.read_csv("./data_processed/whole_project/grouped_all_series_df.csv")
+df_series_name = "./data_processed/whole_project/grouped_all_series_df.csv"
+series = pd.read_csv(df_series_name)
+#echo to consolle - data source contents
+if versobisty >1:
+   print ('\n'*2, df_series_name,os.path.isfile(df_series_name))
+   print ('\n'*2,series.head(),'\n'*2,series.describe())
+
 
 with st.container():
     col1,col2,col3,col4 = st.columns(4)
-    col1.metric(label="duration", value=df["duration"].round(2), delta="5")
+    col1.metric(label="duration", value=df["duration"].round(2), delta="55")
     col2.metric(label="emissions", value=df["emissions"].round(2), delta="1.2")
     col3.metric(label="emissions_rate", value=df["emissions_rate"].round(2), delta="-3.8")
     col4.metric(label="cpu_power", value=df["cpu_power"].round(2), delta="2.4")
@@ -24,27 +40,31 @@ with st.container():
     col7.metric(label="ram_energy", value=df["ram_energy"].round(2), delta="2.9")
     col8.metric(label="energy_consumed", value=df["energy_consumed"].round(2), delta="6.3")
 
-with st.container():
-    st.subheader("Series Data:")
-    st.data_editor(series,
-                column_config={
-                    "date":st.column_config.Column(help="date", width='medium'),
-                    "duration":st.column_config.NumberColumn(help="duration", width='medium'),
-                    "emissions":st.column_config.NumberColumn(help="emissions", width='medium'),
-                    "emissions_rate":st.column_config.NumberColumn(help="emissions_rate", width='medium'),
-                    "cpu_power":st.column_config.NumberColumn(help="cpu_power", width='medium'),
-                    "ram_power":st.column_config.NumberColumn(help="ram_power", width='medium'),
-                    "cpu_energy":st.column_config.NumberColumn(help="cpu_energy", width='medium'),
-                    "ram_energy":st.column_config.NumberColumn(help="ram_energy", width='medium'),
-                    "energy_consumed":st.column_config.NumberColumn(help="energy_consumed", width='medium')
-                }, 
-                hide_index=True, 
-                use_container_width=True
-                )
-    
+
+with st.expander("show data series"):
+
+        
+    with st.container():
+        st.subheader("Series Data:")
+        st.data_editor(series,
+                    column_config={
+                        "date":st.column_config.Column(help="date", width='medium'),
+                        "duration":st.column_config.NumberColumn(help="duration", width='medium'),
+                        "emissions":st.column_config.NumberColumn(help="emissions", width='medium'),
+                        "emissions_rate":st.column_config.NumberColumn(help="emissions_rate", width='medium'),
+                        "cpu_power":st.column_config.NumberColumn(help="cpu_power", width='medium'),
+                        "ram_power":st.column_config.NumberColumn(help="ram_power", width='medium'),
+                        "cpu_energy":st.column_config.NumberColumn(help="cpu_energy", width='medium'),
+                        "ram_energy":st.column_config.NumberColumn(help="ram_energy", width='medium'),
+                        "energy_consumed":st.column_config.NumberColumn(help="energy_consumed", width='medium')
+                    }, 
+                    hide_index=True, 
+                    use_container_width=True
+                    )
+        
     
 st.line_chart(series['duration'])
-st.scatter_chart(series, x=series['duration'],y=series['emissions'])
+st.scatter_chart(series, x='duration', y='emissions')
 
 
 
