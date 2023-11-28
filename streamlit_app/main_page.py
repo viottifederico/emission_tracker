2b000187610dcf894c68da494df6b5d3734d7669
Marco Gambetta <marco.gambetta@gmail.com>
2023-11-28 15:53:48 +0100

    versione 2 - guardabile :-)

--- streamlit_app/main_page.py
+++ streamlit_app/main_page.py
@@ -1,23 +1,30 @@
-import streamlit as st
-import datetime,calendar ,yaml
+import calendar ,yaml, base64, datetime
 import pandas as pd
 import numpy as np
+#streamlit
+import streamlit as st
+from st_clickable_images import clickable_images
+#charting
 import plotly.figure_factory as ff
+import plotly.express as px
 #custom functions 
 from support import *
 
-#print ('\n'*100)
-# Purpse cache configuration and dataframes
-# no input required
-# returns : dict
-#     cnf : dict (configuration)
-#     totals :  pandas df
-#     daily_data: _dict (of pandas df, key is project name)
-#
-# WARNING : HARDCODED CONST (file names)
-#
+print ('\n'*10)
+
+#-----------------------------------------------------------------------------------------------------------
 # @st.cache_data
+#-----------------------------------------------------------------------------------------------------------
 def load_data():
+    '''
+    Purpse cache configuration and dataframes
+       no input required
+       returns : dict
+           cnf : dict (configuration)
+           daily_data: _dict (of pandas df, key is project name)
+           entries : number of rows in daily_data
+       WARNING : HARDCODED CONST (file names)
+    '''
     #assimilate configuration dict
     configuration_filename = 'config.yaml'
     with open(configuration_filename , 'r') as f: _configuration = yaml.safe_load(f)
@@ -27,6 +34,7 @@ def load_data():
     _daily_data_df={}
     for proj in  _configuration['PROJECTS'].values(): 
         _daily_data_df[proj] = pd.read_csv('assimilated_data/'+proj+'.csv')
+        _daily_data_df[proj]['date']= pd.to_datetime(_daily_data_df[proj]['date'])
     return {'cnf':_configuration,'daily_data':_daily_data_df,'entries':len(_daily_data_df[proj])}
   
 
@@ -36,139 +44,146 @@ st.set_page_config(layout="wide")
 
 #load data
 d = load_data()
-#active_features from config file
+# store project names from provided dict by fnc load_data
+projectsName = list(d['daily_data'].keys())
+
+target_proj = projectsName[0]
+i_dt_obj, e_dt_obj = str(d['daily_data'] [target_proj]['date'].iloc[0]),str(d['daily_data'] [target_proj]['date'].iloc[-1])
+i_dt_obj, e_dt_obj = datetime.datetime.strptime(i_dt_obj, "%Y-%m-%d %H:%M:%S"),datetime.datetime.strptime(e_dt_obj, "%Y-%m-%d %H:%M:%S")
+
+#get active_features from config file
 active_features =[af for af in list(d['cnf']['FEATURES'].keys()) if d['cnf']['FEATURES'][af]==True][2::]
-print ('\nactive_features\n',active_features)
-
-
-#retireve whole period initial and final date
-i_dte = list(map(int,d['daily_data'][list(d['daily_data'].keys())[0]]['date'].iloc[0].split('-')))
-e_dte = list(map(int,d['daily_data'][list(d['daily_data'].keys())[0]]['date'].iloc[-1].split('-')))
-#assemple daetime obj required by the double ended slider
-i_dt_obj=datetime.date(i_dte[0],i_dte[1],i_dte[2])
-e_dt_obj=datetime.date(e_dte[0],e_dte[1],e_dte[2])
-
-#define logo
-logo_img_filename = "./resources/rsz_1cf-mod_wip.png"
-log_img = "<img src='data:image/png;base64,{}' class='img-fluid' tooltip='ET Logo'>".format(img_to_bytes(logo_img_filename))
-md_html_string = "<p style='text-align: center; color: grey;'>"+log_img+"</p>" #to be published using st.markdown
-
+active_features.insert(0,'date')
 
 #computes required resources for WHOLE PERIOD zero carbon emission balance
-prj = d['cnf']['PROJECTS']['00']
-print(prj)
+prj = d['cnf']['PROJECTS']['00']  #<--- cfr assimilate-2.py
 cumulated_emmissions = d['daily_data'][prj]['emissions_cumsum'].iloc[-1]
 trees_to_plant_min  = "{0:3.0f}".format(cumulated_emmissions/d['cnf']['CARBON_BALANCE']['tree_co2kg_ratio_min'])
 trees_to_plant_max  = "{0:3.0f}".format(cumulated_emmissions/d['cnf']['CARBON_BALANCE']['tree_co2kg_ratio_max'])
 area_needed = "{0:3.0f}".format(10000*(cumulated_emmissions/d['cnf']['CARBON_BALANCE']['tree_co2kg_ratio_min']) / d['cnf']['CARBON_BALANCE']['trees_per_hectare_avg'])
 
+#-----------------------------------------------------------------------------------------------------------
+# #build page layout
+#-----------------------------------------------------------------------------------------------------------
+#containers for logo (left) and data summary (right)
 
-# define icons for Yearly Balance
-#encode images to be inserted in markdown 
-img_tree = "<img src='data:image/png;base64,{}' class='img-fluid' tooltip='required land to plant the trees'>".format(img_to_bytes("resources/trees.png"))
-img_land = "<img src='data:image/png;base64,{}' class='img-fluid'>".format(img_to_bytes("resources/parcel.png"))
-#build html string to be inserted in the markdown
-summary_tables = '<style>h1 {text-align:center;} h2 {text-align:center;}</style><h1>ZERO EMISSION BALANCE</h1><h2>Y E A R L Y</h2><table width="100%">	<col style="width:30%">	<col style="width:70%">	<tbody>	<tr><td>'+img_land+'</td><td><h2>'+area_needed+' m<sup style="font-size:.8em;">2</sup></h2></td></tr><tr><td>'+img_tree+'</td><td><h2>'+trees_to_plant_max+' - '+trees_to_plant_min+'</h2></td>	</tr></tbody></table>'
-
-#build page layout
-#crerate containers
-col1, col2 = st.columns([2, 1])
-#render logo column
-with col1:
-    st.markdown( md_html_string, unsafe_allow_html=True)
+col1, col2 = st.columns([3, 1])
+#render column 
+with col1: clk =  insert_clickable_image(image_file = "resources/forest0_strip.png", title= "EMISSION TRACKER ver. 2.0", size= 350, justification='center')
 #render summary colum 
 with col2:
-    st.markdown(summary_tables,unsafe_allow_html=True)
+    with st.container(): #row 0 - LEFT TITLE
+        s = '<head><style>h1 {text-align: center;} 2 {text-align: left;} h3 {text-align: center;}</style></head>'
+        st.markdown(s+'<h1>CARBON BALANCE</h1><p><h3> Y E A R  -   t o  -  D A T E</h3>', unsafe_allow_html=True)
+    with st.container(): #row 1 - NUMBER OF TREES to ZERO BALANCE
+        col210, col220 = st.columns([1,3])
+        with col210:    tree_clk = insert_clickable_image(image_file = "resources/trees.png", title= "NUMBER OF TREES to ZERO BALANC", size= 50, justification='right')
+        with col220:    st.markdown('<h1>'+trees_to_plant_min+' - '+trees_to_plant_max+'</h1>', unsafe_allow_html=True)
+    with st.container(): #row 2 - PARCEL AREA FOR TREES to ZERO BALANCE
+        col211, col221 = st.columns([1,3])
+        with col211:    parcel_clk = insert_clickable_image(image_file = "resources/parcel.png", title= "PARCEL AREA FOR TREES to ZERO BALANCE", size= 50, justification='right')
+        with col221:    st.markdown('<h1>'+area_needed+' m<sup style="font-size:.8em;">2</sup></h1>', unsafe_allow_html=True)
+
+# Page span container 
+with st.container(): #slider container
+    slider = st.slider(':green[Select date range]', min_value=i_dt_obj, value=(i_dt_obj, e_dt_obj), max_value=e_dt_obj,  key='DateSlider')
+with st.container(): #Metric container
+           placeholders = st.columns(3)
+           m0 = placeholders[0].empty()
+           m1 = placeholders[1].empty()
+           m2 = placeholders[2].empty()
 
 #build interactive section
 #create containers
 with st.container():
-    col1, col2 = st.columns([1, 4])
-#add selection boxes and log option checkbox
-projectsName = list(d['daily_data'].keys())
-with col1: 
-    log_opt = st.checkbox('Use Logarithmic scale')
-    cumsum_opt = st.checkbox('Cumulated distribution')
-#add double ended slider
-with col2:
-    slider = st.slider(':green[Select date range]', min_value=i_dt_obj, value=(i_dt_obj, e_dt_obj), max_value=e_dt_obj,  key='DateSlider')
-
-
-
-with st.container():
-    col1, col2 = st.columns([1, 4])
-with col1: 
-    selected_projects = st.multiselect('Select the projects to show',projectsName,default=projectsName[-1],key='SelectedProjects_ms')
-    selected_feature = st.selectbox('Select the feature to show',active_features, key='SelectedFeature_sb')
-    selected_chart = st.selectbox('Select chart type',['Daily Line chart :shark:', 'Daily Distribution plot'], key='SelectedChart_sb')
-
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1 :
+        selected_projects = st.multiselect('Select the projects to show',projectsName,default=projectsName[-1],key='SelectedProjects_ms')
+    with col2 : 
+        y_ax_variable = st.selectbox('Select the dependent label (Vertical axis)',active_features[1::],key='Selected_y_lbl_ms')
+    with col3 : 
+        y_ax_variable = st.selectbox('Select the independent label  (Horizontal axis)',active_features,key='Selected_x_lbl_ms')
+    with col4 : 
+        opt_list = ['Linear','SemiLog Y', 'SemiLog X', 'LogLog']
+        options = st.selectbox('Options',opt_list,key='Selected_opt_ms')
+    with col5 : 
+         use_cumulated_values = st.checkbox('Cumulated dist.')
 if len(selected_projects) > 0:
-     res = st.session_state.DateSlider
-     left_ts = calendar.timegm(res[0].timetuple())
-     right_ts = calendar.timegm(res[1].timetuple())
-     left_idx = from_timestamp_get_row(d['daily_data'],left_ts)
-     right_idx = from_timestamp_get_row(d['daily_data'],right_ts)
-     s_df = slice_df(d['daily_data'],left_idx,right_idx)
-     projectNames = list(d['daily_data'].keys())
-     t_df = pd.DataFrame()
-     selected_feature = st.session_state.SelectedFeature_sb
-     if cumsum_opt : 
-        selected_feature=selected_feature+'_cumsum'
-     elif log_opt :
-         selected_feature=selected_feature+'_log'
-     elif log_opt :
-         selected_feature=selected_feature+'_log_cumsum'
-    #
-    #build temp dataframe for charting 
-     for proj in selected_projects:
-         arr, date_arr = from_df_get_values(s_df,proj,selected_feature)
-         if '_cumsum' in selected_feature:
-             arr = arr-arr[0]
-         t_df[proj]=arr
-     t_df['date']=date_arr
+        #get slider values
+        res = st.session_state.DateSlider
+        dict_projects_df_sliced={}
+        for kw,proj in  d['daily_data'].items():
+          dict_projects_df_sliced [kw]= subset_df(proj, column = 'date', values = res)
+        #build data for metrics
+        actual_data = {'emissions':{'label':'emissions', 'title': 'emissions', 'value': dict_projects_df_sliced['All_projects']['emissions_cumsum'].iloc[-1]},
+                       'emissions_rate':{'label':'emissions rate', 'title': 'emissions rate', 'value': np.average(dict_projects_df_sliced['All_projects']['emissions_rate'])},
+                       'energy_consumed':{'label':'energy consumed', 'title': 'energy_consumed', 'value': dict_projects_df_sliced['All_projects']['energy_consumed_cumsum'].iloc[-1]}
+                      }
+        
+        reference_data = {'emissions':{'label':'emissions', 'title': 'emissions', 'value': d['daily_data']['All_projects']['emissions_cumsum'].iloc[-1]},
+                       'emissions_rate':{'label':'emissions rate', 'title': 'emissions rate', 'value': np.average(d['daily_data']['All_projects']['emissions_rate'])},
+                       'energy_consumed':{'label':'energy consumed', 'title': 'energy_consumed' , 'value': d['daily_data']['All_projects']['energy_consumed_cumsum'].iloc[-1]}
+                      }
+        keys = ['emissions', 'emissions_rate', 'energy_consumed']
+        update_metrics(placeholders, actual_data, reference_data, keys, decimals = 2)
 
-     out = s_df['All_projects'][['duration','emissions','emissions_rate','energy_consumed']].sum().to_dict()
-     out['emissions_rate'] = out['emissions_rate']/len(s_df['All_projects']['emissions_rate']) 
-     whole = d['daily_data']['All_projects'][['duration','emissions','emissions_rate','energy_consumed']].sum().to_dict()
-     whole['emissions_rate'] = whole['emissions_rate']/d['entries']
-     with col2:
-     
-         print (type(out), out, whole)
-     #render metrics
-         with st.container():
-        #    col1,col2,col3,col4 = st.columns(4)
-        #    v = "{0:.3f}".format(100*round(out['duration']/whole['duration'],3))+' %'
-        #    col1.metric(label="Total duration".upper(), value=round(out['duration'],2), delta=v)
-        #    v = str(100*round(out['emissions']/whole['emissions'],2))+' %'
-        #    col2.metric(label="Total emissions".upper(), value=round(out['emissions'],2), delta=v)
-        #    v = str(100*round(out['emissions_rate']/whole['emissions_rate'],2))+' %'
-        #    col3.metric(label="Average emissions rate".upper(), value=round(out['emissions_rate'],2), delta=v)
-        #    v = str(100*round(out['energy_consumed']/whole['energy_consumed'],2))+' %'
-        #    col4.metric(label="Total energyconsumed".upper(), value=round(out['energy_consumed'],2), delta=v)
-           col1,col2,col3 = st.columns(3)
-           v = str(100*round(out['emissions']/whole['emissions'],2))+' %'
-           col1.metric(label="Total emissions".upper(), value=round(out['emissions'],2), delta=v)
-           v = str(100*round(out['emissions_rate']/whole['emissions_rate'],2))+' %'
-           col2.metric(label="Average emissions rate".upper(), value=round(out['emissions_rate'],2), delta=v)
-           v = str(100*round(out['energy_consumed']/whole['energy_consumed'],2))+' %'
-           col3.metric(label="Total energyconsumed".upper(), value=round(out['energy_consumed'],2), delta=v)
-         #add chart 
-         if selected_chart in ['Daily Line chart']:
-             lnc =  st.line_chart(t_df, x="date", y=selected_projects)
-        #  elif selected_chart in ['Daily Line chart']:
-        #      lnc = st.line_chart(t_df, x="date", y=selected_projects)
-         elif selected_chart in ['Daily Distribution plot']:
-             hist_data =[]
-             projects = []
-             for proj in t_df.columns: 
-               if proj != 'date': projects.append(proj)
-             for prj in  projects:
-                 hist_data.append(np.array(t_df[prj]))
-             fig = ff.create_distplot(hist_data, projects)    
-             fig.update_layout(legend=dict( yanchor="bottom",y=-0.5,xanchor="right"))
-             fig.update_layout(title_text='Distribution Plot: ' + selected_feature )
-             st.plotly_chart(fig,  use_container_width=True)
-     #add metrics for the selected period
-     #compute values
+        #Parse user options
+        sel_yax_lbl = st.session_state.Selected_y_lbl_ms
+        sel_xax_lbl = st.session_state.Selected_x_lbl_ms
+        opts = st.session_state.Selected_opt_ms
+        yax_lbl = sel_yax_lbl
+        xax_lbl = sel_xax_lbl
+        if not use_cumulated_values:       
+            if 'Linear' in opts : 
+                yax_lbl = sel_yax_lbl
+                xax_lbl = sel_xax_lbl
+            elif 'SemiLog Y' in opts : 
+                yax_lbl=sel_yax_lbl+'_log'
+            elif 'SemiLog X' in opts : 
+                if sel_xax_lbl != 'date':
+                    xax_lbl=sel_xax_lbl+'_log'
+            elif 'LogLog' in opts : 
+                yax_lbl=sel_yax_lbl+'_log'
+                if sel_xax_lbl != 'date':
+                    xax_lbl=sel_xax_lbl+'_log'
+        if use_cumulated_values:       
+            if 'SemiLog Y' in opts : 
+                yax_lbl=sel_yax_lbl+'_log_cumsum'
+            elif 'SemiLog Y' not in opts :      
+                yax_lbl=sel_yax_lbl+'_cumsum'
+            elif 'SemiLog X' in opts : 
+                if sel_xax_lbl != 'date':
+                   xax_lbl=sel_xax_lbl+'_log_cumsum'
+            elif 'SemiLog X' not in opts :      
+                  if sel_xax_lbl != 'date': xax_lbl=sel_xax_lbl+'_cumsum'
+            elif 'LogLog' in opts : 
+                yax_lbl=sel_yax_lbl+'_log_cumsum'
+                if sel_xax_lbl != 'date': xax_lbl=sel_xax_lbl+'_log_cumsum'
+            elif 'LogLog' not in opts : 
+                yax_lbl=sel_yax_lbl+'_cumsum'
+                if sel_xax_lbl != 'date': xax_lbl=sel_xax_lbl+'_cumsum'
 
+        #build temp dataframe for charting 
+        t_df = dict_projects_df_sliced[selected_projects[0]][[xax_lbl]] #take indipendet var from first proj
+        for proj in selected_projects:
+            t_df[proj]= dict_projects_df_sliced[proj][yax_lbl]
+# Chart rendering        
+with st.container():
+    if  len(selected_projects) >0 :    
+        if xax_lbl == 'date':
+            fig = px.scatter(t_df, 
+                            x=xax_lbl, y=selected_projects,  
+                            marginal_y="box",
+                            template="simple_white")
+        else : 
+            fig = px.scatter(t_df, 
+                            x=xax_lbl, y=selected_projects,  
+                            marginal_y="box",
+                            marginal_x="box", 
+                            trendline="ols", 
+                            template="simple_white")
+                  
+        st.plotly_chart(fig,  use_container_width=True)
+               
+        
 
--- streamlit_app/support.py
+++ streamlit_app/support.py
@@ -1,13 +1,21 @@
 import datetime,calendar, yaml 
 import numpy as np
-from base64 import b64encode
-
+import base64
 
 import streamlit as st
 from streamlit_extras.dataframe_explorer import dataframe_explorer 
+from st_clickable_images import clickable_images
+
 import pandas as pd
 
 def build_subpage(streamlit_obj, title,df_csv):
+    """_summary_
+
+    Args:
+        streamlit_obj (_type_): _description_
+        title (_type_): _description_
+        df_csv (_type_): _description_
+    """
     streamlit_obj.set_page_config(layout="wide")
     streamlit_obj.title(title)
     df = pd.read_csv(df_csv)
@@ -26,18 +34,77 @@ def build_subpage(streamlit_obj, title,df_csv):
     
 
 
+def update_metrics(placeholders, actual_data, reference_data, keys, decimals = 2):
+    """_summary_
+
+    Args:
+        placeholders (list): list of streamlit columns object
+        actual_data (dict): actual data {key:{label: str, title: str, value: numeric }} see example
+        reference_data (): ref data {key:{label: str, title: str, value: numeric }} see example
+        keys (list):sorted list must match dictionary 0 level keys
+        
+        dictionary example : 
+        {
+         'emissions':{'label':'Emissions', title:'EMISSIONS, 'value':1234.4321},
+         'energy consumed':{'label':'Energy consumed', title:'CONSUMED NRGY, 'value':5678.321} 
+        }
+    """
+    for i,placeholder in enumerate(placeholders):
+        keyw = keys[i] 
+        actual_element =  actual_data[keyw]
+        ref_element = reference_data[keyw] 
+        act_value = round(actual_element['value'],2)
+        act_label = actual_element['label'].upper()
+        act_title  = actual_element['title']
+        ref_value = round(ref_element['value'],2)
+        ref_label = ref_element['label'].upper()
+        ref_title  = ref_element['title']
+        v = str(100*round(act_value/ref_value,decimals))+' %'
+        placeholder.metric(label=act_label.upper(), value=act_value, delta=v)
+
 '''
  Image tools
 '''
-# purpose : read image and returns its bytes repr (required to insert an image in streamlit markdown html)
-# input : image file
-#output : byte reprsentation
-# 
 def img_to_bytes(img_path):
+    """_summary_
+    Read local image file and returns its bytes repr (required to insert an image in streamlit markdown html)
+
+    Args:
+        img_path (str): local image filename
+
+    Returns:
+        str: encoded image
+    """
     with open(img_path, 'rb') as f:
          img_bytes = f.read()
-    encoded = b64encode(img_bytes).decode()
+    encoded = base64.b64encode(img_bytes).decode()
     return encoded
+
+def insert_clickable_image(image_file, title= "", size= 100, justification='center'):
+    """_summary_
+    
+    Insert a clickable image in stramlit container 
+
+    Args:
+        image_file (str, required): local image file
+        title (str, optional): image caption, also it is the tooltip. Defaults to "".
+        size (int, optional): image size Defaults to 100.
+        justification (str, optional): image location. Defaults to 'center'.
+
+    Returns:
+        obj: image title if clicked
+    """
+    images = []
+    for file in [image_file]:
+        with open(file, "rb") as image:
+              encoded = base64.b64encode(image.read()).decode()
+              images.append(f"data:image/jpeg;base64,{encoded}")
+
+    clicked = clickable_images(images,titles=[title],
+                                       div_style={"display": "flex", "justify-content": justification, "flex-wrap": "wrap"},
+                                       img_style={"margin": "5px", "height": str(size)+"px"},)
+    return clicked
+
 
 '''
 Dataframe operations
@@ -72,20 +139,22 @@ def from_dict_get_value(dictionary = None, keyword = None, roundup_digits = 2):
 #
 def from_df_get_values(daily_df_dict = None, projectName = None,columnName = None):
     arr = np.array(daily_df_dict[projectName][columnName])
-    datetime_arr = np.array(daily_df_dict[projectName]['date'])
+    datetime_arr = daily_df_dict[projectName]['date']
     return arr,datetime_arr
 
-# purpose slice all daily df accordingly to left and right indexes
-# input : 
-#        daily df dictionary : dict,  which contains dataframes of each project, key is project name
-#        left_index : int,  inital cutting point
-#        right_index : int,  final cutting point
-# output: 
-#        daily df dictionary where each df is sliced
-def slice_df(daily_df_dict, left_index,right_index):
-    indices = list(range(left_index,right_index-1))
-    sliced_df={}
-    projecNames = list(daily_df_dict.keys())
-    for proj in projecNames:
-        sliced_df[proj]= daily_df_dict[proj].iloc[indices]
-    return sliced_df    
+
+def subset_df(df, column='date', values=None):
+
+    """subset a dataframe given column name and boundary values
+
+    Args:
+        df (DataFramem):Pandas dataframe
+        column (str): column name, must exists in  df
+        values (same as df.column): boundary values of the same type of df.column
+
+    Returns:
+        DataFrame : subset of original df
+    """
+    out = df[(df[column] > values[0]) & (df[column]< values[1])]
+    print ('-> subset_df ::', out[column].iloc[0], type(out[column].iloc[0]),out[column].iloc[-1], type(out[column].iloc[-1]), )
+    return out
